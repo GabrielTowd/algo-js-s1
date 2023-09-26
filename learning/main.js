01e724455e00620023cd19206a093573306ca273
Gabriel Monier <40315281+GabrielTowd@users.noreply.github.com>
2023-09-26 17:36:22 +0200

    adding learning and exercice 1 & 2

---
+++ learning/main.js
@@ -0,0 +1,17 @@
+//alert("Bonjour, tu veux quitter ce site ?")
+
+//console.log("bienvenue dans la console")
+
+var cardName = "Choixpeau"
+var age = 12
+var list = []
+var object = {}
+
+age = age + 16
+
+if (6 !== 7 && 6 < 7) {
+    console.log('dlfgjkh')
+}
+else {
+    console.log('dflkjdfgskl')
+}
